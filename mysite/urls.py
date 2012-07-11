812946e04a45659c328041180d650a860540f138
chengl6500 <lcmust@yahoo.com.cn>
2012-07-11 12:33:24 +0800

    mdf<mysite:html,css,py>

--- mysite/urls.py
+++ mysite/urls.py
@@ -12,18 +12,22 @@ publisher_info = {
 #    'template_object_name':'publisher',
 }
 
-urlpatterns = patterns('mysite.polls.views',
+urlpatterns = patterns('',
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #django-css
+    #url(r'^static-django/css/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'/usr/local/lib/python2.6/dist-packages/django/contrib/admin/media/css/'}),
+    #django-jpg|png
+    #url(r'^static-django/img/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'/usr/local/lib/python2.6/dist-packages/django/contrib/admin/media/css/'}),
+)
+
+urlpatterns += patterns('mysite.polls.views',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     url(r'publishers/$', list_detail.object_list, publisher_info),
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'env'),
+    url(r'^$', 'home1'),
     url(r'^time/$', 'current_datetime'),
     url(r'^time/(\d{1,2})/$', 'hours_ahead'),
     url(r'^now_furture/(\d{1,2})/$', 'now_furture'),
@@ -39,13 +43,12 @@ urlpatterns = patterns('mysite.polls.views',
     url(r'^login_auth/$', 'login_auth'),
     url(r'^logout/$', 'logout'),
     url(r'^register/$', 'register'),
-    url(r'^user_all/$', 'django_user_all'),
-    #django-css
-    #url(r'^css/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'/usr/local/lib/python2.6/dist-packages/django/contrib/admin/media/css/'}),
-    #django-jpg|png
-    #url(r'^img/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'/usr/local/lib/python2.6/dist-packages/django/contrib/admin/media/css/'}),
+    #url(r'^user_all/$', 'django_user_all'),
+)
+
+urlpatterns += patterns('',
     #myapp-css
-    url(r'^css/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'/home/love/cl-dep/mysite/templates/static/css/'}),
-    #myapp-jpg|png
-    #url(r'^static/img/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'/home/love/cl-dep/mysite/templates/static/img/'}),
+    url(r'^static/css/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'/home/love/cl-dep/mysite/templates/static/css/'}),
+    #myapp-jpg|png|pig|...
+    url(r'^static/img/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'/home/love/cl-dep/mysite/templates/static/img/'}),
 )
