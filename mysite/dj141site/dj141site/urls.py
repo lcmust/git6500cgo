07b3e03379f5a9472b5759acad8f6d60a9a18731
chengl6500 <lcmust@yahoo.com.cn>
2012-11-04 04:50:51 +0800

    add some .pys

--- mysite/dj141site/dj141site/urls.py
+++ mysite/dj141site/dj141site/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.http import HttpResponse
+import dj141site.settings
+dj141site_path = str(dj141site.settings.TEMPLATE_DIRS[0])
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -20,8 +23,13 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     ###my_url:
-    url(r'^$', include('app1.urls')),
-    url(r'^test/$', 'app1.views.hello'),
-    url(r'^a/$', 'app1.views.index'),
+    url(r'^app1/', include('app1.urls')),
+    url(r'^index/$', 'app1.views.index'),
+    url(r'^test/$', 'app1.views.test', {'test1':dj141site_path}),
+
+    url(r'^static1/css/(?P<path>.*)$', 'django.views.static.serve', {'document_root':dj141site_path + '/static1/css/'}),
+    url(r'^static1/img/(?P<path>.*)$', 'django.views.static.serve', {'document_root':dj141site_path + '/static1/img/'}),
+    url(r'^static1/js/(?P<path>.*)$', 'django.views.static.serve', {'document_root':dj141site_path + '/static1/js/'}),
+    ###(r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root' : '/path/to/my/files/'})
 
 )
